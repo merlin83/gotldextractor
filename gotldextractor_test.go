84971c1ad25ebc87bef4387c3623e35abad0d785
rubik <rubik@galadriel.localdomain>
2013-11-30 18:15:52 +0800

    Write proper testing for hostnames

--- gotldextractor_test.go
+++ gotldextractor_test.go
@@ -1,7 +1,7 @@
 package gotldextractor
 
 import (
-	"fmt"
+	_ "fmt"
 	"testing"
 )
 
@@ -21,30 +21,63 @@ func TestFetchEffectiveTLDNames(t *testing.T) {
 	}
 }
 
+func invalidParseResult(t *testing.T, r *TLDResult) {
+	t.Error("invalid parse result for ", r.GetHostname(), " subdomain: ", r.Subdomain, " domain: ", r.Domain, " tld: ", r.TLD)
+}
+
 func TestParseHosts(t *testing.T) {
 	tldextract := TLDExtractor{}
 	_, err := tldextract.Build()
 	if err != nil {
 		t.Error("Error building tree! - ", err)
 	}
-	HOSTNAMES := []string{
-		"meh.thisshouldwork.ck",
-		"meh.www.ck",
-		"www.guy.kawasaki.jp",
-		"www.city.kawasaki.jp",
-		"www.cnn.com",
-		"www.bbc.co.uk",
-		"www.sina.com.cn",
-		"weibo.sina.com.cn",
-		"news.ycombinator.com",
-		"www.github.com",
-		"www.github.com:443",
-		"www.facebook.com:8080"}
-	for _, hostname := range HOSTNAMES {
-		r, err := tldextract.ParseHost(hostname)
-		fmt.Println(r)
-		if err != nil {
-			t.Error(r)
-		}
+	var r TLDResult
+	r, err = tldextract.ParseHost("meh.thisshouldwork.ck")
+	if r.Subdomain != "" && r.Domain != "meh" && r.TLD != "thisshouldwork.ck" {
+		invalidParseResult(t, &r)
+	}
+	r, err = tldextract.ParseHost("meh.www.ck")
+	if r.Subdomain != "meh" && r.Domain != "www" && r.TLD != "ck" {
+		invalidParseResult(t, &r)
+	}
+	r, err = tldextract.ParseHost("www.guy.kawasaki.jp")
+	if r.Subdomain != "" && r.Domain != "www" && r.TLD != "guy.kawasaki.jp" {
+		invalidParseResult(t, &r)
+	}
+	r, err = tldextract.ParseHost("www.city.kawasaki.jp")
+	if r.Subdomain != "www" && r.Domain != "city" && r.TLD != "kawasaki.jp" {
+		invalidParseResult(t, &r)
+	}
+	r, err = tldextract.ParseHost("www.cnn.com")
+	if r.Subdomain != "www" && r.Domain != "cnn" && r.TLD != "com" {
+		invalidParseResult(t, &r)
+	}
+	r, err = tldextract.ParseHost("www.bbc.co.uk")
+	if r.Subdomain != "www" && r.Domain != "bbc" && r.TLD != "co.uk" {
+		invalidParseResult(t, &r)
+	}
+	r, err = tldextract.ParseHost("www.sina.com.cn")
+	if r.Subdomain != "www" && r.Domain != "sina" && r.TLD != "com.cn" {
+		invalidParseResult(t, &r)
+	}
+	r, err = tldextract.ParseHost("weibo.sina.com.cn:8080")
+	if r.Subdomain != "weibo" && r.Domain != "sina" && r.TLD != "com.cn" {
+		invalidParseResult(t, &r)
+	}
+	r, err = tldextract.ParseHost("news.ycombinator.com")
+	if r.Subdomain != "news" && r.Domain != "ycombinator" && r.TLD != "com" {
+		invalidParseResult(t, &r)
+	}
+	r, err = tldextract.ParseHost("www.github.com")
+	if r.Subdomain != "www" && r.Domain != "github" && r.TLD != "com" {
+		invalidParseResult(t, &r)
+	}
+	r, err = tldextract.ParseHost("www.github.com:443")
+	if r.Subdomain != "www" && r.Domain != "github" && r.TLD != "com" {
+		invalidParseResult(t, &r)
+	}
+	r, err = tldextract.ParseHost("www.facebook.com:8080")
+	if r.Subdomain != "www" && r.Domain != "facebook" && r.TLD != "com" {
+		invalidParseResult(t, &r)
 	}
 }
